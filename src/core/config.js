e4ee20da8e428c730a49f3f0dba09cae8a6f61a9
drajk <dheerajkhs@gmail.com>
2020-08-10 01:11:44 +1000

    feat: unit test for valid title

--- src/core/config.js
+++ src/core/config.js
@@ -1,2 +1,3 @@
+// TODO: get this from env config
 export const SUBSCRIPTION_ENDPOINT =
   "https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth";
--- src/layouts/__tests__/Page.test.js
+++ src/layouts/__tests__/Page.test.js
@@ -7,9 +7,22 @@ const getWrapper = (title) => render(<Page title={title} />);
 
 describe("<Page />", () => {
   it("should not show header and footer without title", () => {
+    // arrange and act
     const { queryAllByTestId } = getWrapper(null);
 
+    // assert
     expect(queryAllByTestId("page-header")).toHaveLength(0);
     expect(queryAllByTestId("page-footer")).toHaveLength(0);
+  });
+
+  it("should show header and footer with title", () => {
+    // arrange and act
+    const { queryAllByTestId, getAllByText } = getWrapper("some title");
+
+    expect(queryAllByTestId("page-header")).toHaveLength(1);
+    expect(queryAllByTestId("page-footer")).toHaveLength(1);
+
+    // assert
+    expect(getAllByText(/some title/i)).toHaveLength(2);
   });
 });
